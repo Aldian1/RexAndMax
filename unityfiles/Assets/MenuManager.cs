4766b633694ec60f2e3d4f9b8c6857bdcf6625dd
Aldian1 <Aldian@hotmail.co.uk>
2016-04-03 06:03:22 +0100

    -Menu Implementations 	-News Feed 	-Options 	-Start, quit 	-Level Select

---
+++ unityfiles/Assets/MenuManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void OpenWebsite()
+	{
+		Application.OpenURL("http://maxandrex.com/");
+	}
+}
