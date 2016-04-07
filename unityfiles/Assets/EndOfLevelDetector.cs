5ab9854aa96276b49fb661047918fcaad17a30de
Aldian1 <Aldian@hotmail.co.uk>
2016-04-07 20:17:24 +0100

    Small Update - 12:18pm - 07/04/2016
    Im going to have a new format for git updates now. [Title] - [Time] - [Date]
    Makes things a little cleaner.
    
    Added:
    -Deaths and score are now updated appropriately
    -Stars are linked to both the main menu level select and on the players screen
    -Fixed a small bug with collisions
    -Buttons now work on the end of level screen
    
    TODO:
    
    Enemies still (this afternoon)
    Figure out how score + lives = star amount

--- unityfiles/Assets/EndOfLevelDetector.cs
+++ unityfiles/Assets/EndOfLevelDetector.cs
@@ -1,10 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class EndOfLevelDetector : MonoBehaviour {
 
+	public int nextleveltoload;
+
+	public GameObject[] stars;
 	void OnTriggerEnter2D(Collider2D col)
 	{
 		GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<LevelController>().GO();
+	}
+
+
+	public void ButtonOption(Button button){
+		if (button.name == "MainMenu") {
+			SceneManager.LoadScene (0);
+		}
+
+		if (button.name == "NextLevel") {
+			SceneManager.LoadScene (nextleveltoload);
+
+
+		}
 	}
 }
