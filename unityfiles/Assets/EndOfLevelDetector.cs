ec5af65adc2969adad54fdd46f97109fe88acf3f
Aldian1 <Aldian@hotmail.co.uk>
2016-04-07 06:15:57 +0100

    07/04/2016 pt 2 With the help of rayanne the level has expanded a bit. We are now using scene fusion. A couple of scripts were on certain prefabs when they werent meant to be so i have deleted them. Added: -End of level screen -Lives -Score
    TODO:
    
    Hook up the buttons and scores so its more visual
    add some enemys to gain points from
    hook up the lives
    figure out what the player needs to get to attain so many stars etc.
    
    ^^^^^^^^^^^ will do this tomorrow ^^^^^^^^^^^

---
+++ unityfiles/Assets/EndOfLevelDetector.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndOfLevelDetector : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<LevelController>().GO();
+	}
+}
---
+++ unityfiles/Assets/LevelController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LevelController : MonoBehaviour {
+
+
+	public float score;
+	public int lives = 3;
+	public GameObject endoflevelMarker;
+
+	public GameObject endoflevelscreen;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void GO()
+	{
+		endoflevelscreen.SetActive (true);
+	}
+}
