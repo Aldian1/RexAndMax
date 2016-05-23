9eb3e66c4aaa149eb3305865576fd45335abc15e
Aldian1 <Aldian@hotmail.co.uk>
2016-05-23 16:32:38 +0100

    Added a bunch of crap for meeting

---
+++ unityfiles/Assets/WheelPlatform.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WheelPlatform : MonoBehaviour {
+
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            col.transform.parent = this.transform;
+            Debug.Log("1");
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            col.transform.parent = null;
+        }
+    }
+}
